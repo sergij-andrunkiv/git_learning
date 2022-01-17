58d4deb70d667e5aaf64fb835df37408b4878d00
Sergij Andrunkiv <sergij.andrunkiv@gmail.com>
2022-01-18 01:06:11 +0200

    Added function minus

--- first/nums.py
+++ first/nums.py
@@ -1,6 +1,14 @@
-def plu(a, b):
+def plus(a, b):
     return a + b
 
 
+text = 'new text'
+
+
+def minus(a,b):
+    return a - b
+
+
 if __name__ == '__main__':
-    print(plu(2, 2, ))
+    print(plus(2, 2))
+    print(minus(5,2))
